e6203912e24a396933f680352c5756967b69ebb3
Leonardo <rufinodevelopment@gmail.com>
2025-01-08 15:23:42 -0300

    chore: Initial commit
    Includes basic project structure and initial configuration

---
+++ app/src/main/java/com/rufino/ididit/AddTaskFragment.kt
@@ -0,0 +1,11 @@
+package com.rufino.ididit
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AddTaskFragment : Fragment(R.layout.fragment_home) {
+
+}
---
+++ app/src/main/java/com/rufino/ididit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rufino.ididit
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
